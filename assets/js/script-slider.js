fb4ce89dab0756b42860ed8ae8de631ec4c02d72
Daniel Miranda <Dm2639532@gmail.com>
2023-07-10 17:19:39 -0300

    Os sliders estão todos funcionais

--- assets/js/script-slider.js
+++ assets/js/script-slider.js
@@ -3,6 +3,12 @@ const btnAnterior = document.getElementById('btn-anterior');
 const btnProximo = document.getElementById('btn-proximo');
 let slideAtivo = 0;
 
+const imagens = document.getElementById('img')
+const imagenPortifolio = document.querySelectorAll('.portifolio-carrosel img');
+const btnAnteriorPortifolio = document.getElementById('btn-anterior-portifolio')
+const btnProximoPortifolio = document.getElementById('btn-proximo-portifolio')
+let idx = 0;
+
 function esconderSlider(){
     slider.forEach(item => item.classList.remove('on'))
 }
@@ -31,7 +37,27 @@ function anteriorSlider() {
     mostrarSlider()
 }
 
+function portifolioSliderProximo(){
+    idx++;
+    if(idx > imagenPortifolio.length -3){
+        idx = 0;
+    }
+    imagens.style.transform = `translateX(${-idx * 400}px)`;
+}
+
+function portifolioSliderAnterior(e){
+    idx--;
+    if(idx < imagenPortifolio.length -1 ){
+        idx = 0;
+    }
+    imagens.style.transform = `translateX(${-idx * 400}px)`;
+}
+
+
 setInterval(proximoSlider, 5000)
 
 btnAnterior.addEventListener('click', anteriorSlider)
 btnProximo.addEventListener('click', proximoSlider)
+
+btnAnteriorPortifolio.addEventListener('click', portifolioSliderAnterior)
+btnProximoPortifolio.addEventListener('click', portifolioSliderProximo)
