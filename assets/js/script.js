a114094e741d7f682254c4b6c1ecb1bd1bd2d62a
Rafael Gaspar <contato@rafaelgaspar.com.br>
2023-07-10 21:30:51 -0300

    Interação card carros
    Adicionado interação nos cards dos carros, incluindo um Popup ao clicar em Saiba Mais

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
 //Definição de variáveis / Captura de elementos DOM
 let btns = document.getElementsByClassName('botoes-nav');
 
+let bmw = document.getElementById('bmw');
+let lambo = document.getElementById('lambo');
+let porsche = document.getElementById('porsche');
+let chevy = document.getElementById('chevy');
+let supra = document.getElementById('supra');
+let mustang = document.getElementById('mustang');
+let audi = document.getElementById('audi');
+
+let popBMW = document.getElementById('listagem-bmw-aberto')
+let popLambo = document.getElementById('listagem-lambo-aberto')
+let popPorsche = document.getElementById('listagem-porsche-aberto')
+let popChevy = document.getElementById('listagem-chevy-aberto')
+let popSupra = document.getElementById('listagem-supra-aberto')
+let popMustang = document.getElementById('listagem-mustang-aberto')
+let popAudi = document.getElementById('listagem-audi-aberto')
+
+let fechar = document.getElementsByClassName('fechar')
+let popup = document.getElementsByClassName('popup')
+
 //Funções
 
 let btnUm = (e)=>{
@@ -18,6 +37,30 @@ let btnTres = (e)=>{
     btns[2].style.transition = 'all 0.1s'
     btns[2].style.boxShadow = '0px 1px 6px black'
 };
+
+bmw.addEventListener('click', ()=>{
+    popBMW.style.display = 'flex';
+})
+lambo.addEventListener('click', ()=>{
+    popLambo.style.display = 'flex';
+})
+porsche.addEventListener('click', ()=>{
+    popPorsche.style.display = 'flex';
+})
+chevy.addEventListener('click', ()=>{
+    popChevy.style.display = 'flex';
+})
+supra.addEventListener('click', ()=>{
+    popSupra.style.display = 'flex';
+})
+mustang.addEventListener('click', ()=>{
+    popMustang.style.display = 'flex';
+})
+audi.addEventListener('click', ()=>{
+    popAudi.style.display = 'flex';
+})
+
+
 
 //Eventos
 
