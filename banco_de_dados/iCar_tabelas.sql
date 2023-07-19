0124ad1e64bfbf426133fe22f4170fc5b8d970e3
Daniel Miranda <Dm2639532@gmail.com>
2023-07-19 15:27:11 -0300

    Adição da pasta banco_de_dados e um arquivo SQL

---
+++ banco_de_dados/iCar_tabelas.sql
@@ -0,0 +1,77 @@
+CREATE TABLE cliente(
+ CPF INT PRIMARY KEY not NULL,
+ Nome VARCHAR(60) NOT NULL,
+ RG INT not NULL,
+ data_de_Nascimento INT not NULL,  
+ estado_civil VARCHAR(12) not NULL,  
+ genero VARCHAR(20) not NULL,
+ idGerente INT,
+ FOREIGN KEY(idGerente) REFERENCES gerente(idGerente)
+);
+
+CREATE TABLE contatos(
+ id_contatos SERIAL PRIMARY KEY,
+ Telefone INT NOT NULL,  
+ Telefone_2 INT,
+ Email VARCHAR(260) not NULL,
+ CPF INT,
+ FOREIGN KEY(CPF) REFERENCES cliente(CPF)
+);
+
+CREATE TABLE endereco(
+ id_endereco SERIAL PRIMARY KEY,
+ CEP INT not NULL,  
+ UF VARCHAR(2) not NULL,  
+ Rua VARCHAR(150) not NULL,  
+ Cidade VARCHAR(80) not NULL,
+ CPF INT,
+ FOREIGN KEY(CPF) REFERENCES cliente(CPF)
+);
+
+CREATE TABLE carros(
+ renavam INT PRIMARY KEY  NOT NULL,
+ chassi INT  NOT NULL,  
+ placa VARCHAR(7)  NOT NULL,  
+ modelo VARCHAR(60)  NOT NULL,  
+ marca VARCHAR(50)  NOT NULL,     
+ IPVA INT  NOT NULL,  
+ seguro INT  NOT NULL,  
+ preco INT  NOT NULL,  
+ ano_modelo INT  NOT NULL,  
+ ano_de_fabricacao INT  NOT NULL,    
+ motor INT  NOT NULL,    
+ revisoes INT  NOT NULL,
+ CPF INT,
+ idGerente INT,
+ FOREIGN KEY(CPF) REFERENCES cliente(CPF),
+ FOREIGN KEY(idGerente) REFERENCES gerente(idGerente)
+); 
+
+CREATE TABLE especificacoes(
+  id_especificacoes SERIAL PRIMARY KEY,
+  cor VARCHAR(40)  NOT NULL,
+  combustível VARCHAR(20)  NOT NULL,
+  capacidade_passageiros INT  NOT NULL,
+  caixa_de_marcha VARCHAR(30)  NOT NULL,
+  potencia INT  NOT NULL,
+  renavam INT,
+  FOREIGN KEY(renavam) REFERENCES carros(renavam)
+);
+
+CREATE TABLE Cota( 
+ numero_de_Cotas INT NOT NULL,  
+ id_cotas SERIAL PRIMARY KEY,  
+ valor_cotas INT NOT NULL,  
+ CPF INT,  
+ idGerente INT,  
+ renavam INT,
+ FOREIGN KEY(CPF) REFERENCES cliente(CPF),
+ FOREIGN KEY(idGerente) REFERENCES gerente(idGerente),
+ FOREIGN KEY(renavam) REFERENCES carros(renavam)
+); 
+
+CREATE TABLE gerente( 
+ idGerente INT PRIMARY KEY NOT NULL,  
+ funcao VARCHAR(40) NOT NULL,  
+ nome VARCHAR(60) NOT NULL
+); 
